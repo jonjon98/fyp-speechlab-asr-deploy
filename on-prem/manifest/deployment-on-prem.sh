f5f702566643e68396f8cef9a5ddc6e766d14b10
jonjon98 <yapzh.jon@gmail.com>
2022-07-19 18:42:32 +0800

    feat: adds ingress resource and controller as well as reorganises the on-prem/manifest directory

--- on-prem/manifest/deployment-on-prem.sh
+++ on-prem/manifest/deployment-on-prem.sh
@@ -1,35 +1,67 @@
 #!/bin/bash
 
-if [ "$1" = 'up' ]; then
-    echo "Applying ASR project from K8s cluster"
+if [ "$1" = 'project' ]; then
+    if [ "$2" = 'up' ]; then
+        echo "Applying ASR project from K8s cluster"
+
+        kubectl apply -f default/decoding-sdk-server-service.yaml
+        kubectl apply -f default/decoding-sdk-server-deployment.yaml
+        kubectl apply -f default/decoding-sdk-worker-storageclass.yaml
+        kubectl apply -f default/decoding-sdk-worker-persistentvolume.yaml
+        kubectl apply -f default/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
+        kubectl apply -f default/decoding-sdk-worker-deployment.yaml
     
-    kubectl apply -f decoding-sdk-server-service.yaml
-    kubectl apply -f decoding-sdk-server-deployment.yaml
-    kubectl apply -f decoding-sdk-worker-storageclass.yaml
-    kubectl apply -f decoding-sdk-worker-persistentvolume.yaml
-    kubectl apply -f decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
-    kubectl apply -f decoding-sdk-worker-deployment.yaml
+    elif  [ "$2" = 'down' ]; then
+        echo "Removing ASR project from K8s cluster"
 
-elif  [ "$1" = 'down' ]; then
-    echo "Removing ASR project from K8s cluster"
+        kubectl delete -f default/decoding-sdk-server-deployment.yaml
+        kubectl delete -f default/decoding-sdk-server-service.yaml
+        kubectl delete -f default/decoding-sdk-worker-deployment.yaml
+        kubectl delete -f default/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
+        kubectl delete -f default/decoding-sdk-worker-persistentvolume.yaml
+        kubectl delete -f default/decoding-sdk-worker-storageclass.yaml
 
-    kubectl delete -f decoding-sdk-server-deployment.yaml
-    kubectl delete -f decoding-sdk-server-service.yaml
-    kubectl delete -f decoding-sdk-worker-deployment.yaml
-    kubectl delete -f decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
-    kubectl delete -f decoding-sdk-worker-persistentvolume.yaml
-    kubectl delete -f decoding-sdk-worker-storageclass.yaml
+    fi
+
+elif  [ "$1" = 'ingress-controller' ]; then
+    if [ "$2" = 'up' ]; then
+        echo "Applying ASR Ingress Controller from K8s cluster"
+
+        kubectl apply -f ingress/ingress-nginx-controller.yaml
+
+    elif  [ "$2" = 'down' ]; then
+        echo "Removing ASR Ingress Controller from K8s cluster"
+
+        kubectl delete -f ingress/ingress-nginx-controller.yaml
+
+    fi
+
+elif  [ "$1" = 'ingress-resource' ]; then
+    if [ "$2" = 'up' ]; then
+        echo "Applying ASR Ingress Resource from K8s cluster"
+
+        kubectl apply -f ingress/decoding-sdk-server-ingress.yaml
+
+    elif  [ "$2" = 'down' ]; then
+        echo "Removing ASR Ingress Resource from K8s cluster"
+
+        kubectl delete -f ingress/decoding-sdk-server-ingress.yaml
+
+    fi
 
 elif  [ "$1" = 'help' ]; then
-    echo "Usage ./deployment-on-prem.sh <up/down/help>"
-    echo "  up: applies ASR projec to K8s cluster"
-    echo "  down: removes ASR project from K8s cluster"
+    echo "Usage ./deployment-on-prem.sh [project/ingress-controller/ingress-resource/help] [up/down]"
+    echo "  project: project component"
+    echo "  ingress-controller: ingress-controller component"
+    echo "  ingress-resource: ingress-resource component"
     echo "  help: displays this menu"
+    echo "  up: deploys component to K8s cluster (only applies to components)"
+    echo "  down: removes component from K8s cluster (only applies to components)"
     exit
 
 else
     echo "Not a vaild flag"
-    echo "Usage ./build.sh <up/down/scale/help>"
+    echo "Usage ./build.sh [project/ingress/help] [up/down]"
     exit
 
 fi
