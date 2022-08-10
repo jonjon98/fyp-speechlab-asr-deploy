3f512165f095e586eb569008ea1fef9a3b9d6244
jonjon98 <yapzh.jon@gmail.com>
2022-08-10 16:14:48 +0800

    chore: reformats the k8s manifest files for a cleaner project structure.

--- on-prem/manifest/deployment-on-prem.sh
+++ on-prem/manifest/deployment-on-prem.sh
@@ -4,22 +4,22 @@ if [ "$1" = 'project' ]; then
     if [ "$2" = 'up' ]; then
         echo "Applying ASR project from K8s cluster"
 
-        kubectl apply -f default/decoding-sdk-server-service.yaml
-        kubectl apply -f default/decoding-sdk-server-deployment.yaml
-        kubectl apply -f default/decoding-sdk-worker-storageclass.yaml
-        kubectl apply -f default/decoding-sdk-worker-persistentvolume.yaml
-        kubectl apply -f default/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
-        kubectl apply -f default/decoding-sdk-worker-deployment.yaml
+        kubectl apply -f services/decoding-sdk-server-service.yaml
+        kubectl apply -f deployments/decoding-sdk-server-deployment.yaml
+        kubectl apply -f volumes/decoding-sdk-worker-storageclass.yaml
+        kubectl apply -f volumes/decoding-sdk-worker-persistentvolume.yaml
+        kubectl apply -f volumes/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
+        kubectl apply -f deployments/decoding-sdk-worker-deployment.yaml
     
     elif  [ "$2" = 'down' ]; then
         echo "Removing ASR project from K8s cluster"
 
-        kubectl delete -f default/decoding-sdk-server-deployment.yaml
-        kubectl delete -f default/decoding-sdk-server-service.yaml
-        kubectl delete -f default/decoding-sdk-worker-deployment.yaml
-        kubectl delete -f default/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
-        kubectl delete -f default/decoding-sdk-worker-persistentvolume.yaml
-        kubectl delete -f default/decoding-sdk-worker-storageclass.yaml
+        kubectl delete -f deployments/decoding-sdk-server-deployment.yaml
+        kubectl delete -f services/decoding-sdk-server-service.yaml
+        kubectl delete -f deployments/decoding-sdk-worker-deployment.yaml
+        kubectl delete -f volumes/decoding-sdk-worker-claim0-persistentvolumeclaim.yaml
+        kubectl delete -f volumes/decoding-sdk-worker-persistentvolume.yaml
+        kubectl delete -f volumes/decoding-sdk-worker-storageclass.yaml
 
     fi
 
@@ -61,7 +61,7 @@ elif  [ "$1" = 'help' ]; then
 
 else
     echo "Not a vaild flag"
-    echo "Usage ./build.sh [project/ingress/help] [up/down]"
+    echo "Usage ./deployment-on-prem.sh [project/ingress-controller/ingress-resource/help] [up/down]"
     exit
 
 fi
